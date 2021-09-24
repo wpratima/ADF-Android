890d666dbd1a4016c1525180b3a73b310c5471b2
Pratima <pratimawaghmare44@gmail.com>
2021-09-24 21:16:20 +0530

    Added functionality to fetch search url from different browsers

--- AccessibilityServiceCheck/app/src/main/java/com/example/accessibilityservicecheck/MyService.java
+++ AccessibilityServiceCheck/app/src/main/java/com/example/accessibilityservicecheck/MyService.java
@@ -2,29 +2,115 @@ package com.example.accessibilityservicecheck;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Browser;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityWindowInfo;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MyService extends AccessibilityService {
     private String TAG = "MyService";
+    private HashMap<String, Long> previousUrlDetections = new HashMap<>();
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.d("event", String.valueOf(event.getEventTime()));
-        Log.d("event", String.valueOf(event));
-        Log.e(TAG, "onAccessibilityEvent: ");
+        //Log.e(TAG, "onAccessibilityEvent: ");
+        //Log.d("event", String.valueOf(event.getEventTime()));
+        //Log.d("event", String.valueOf(event));
+
+
+        //to fetch the url
+        AccessibilityNodeInfo parentNodeInfo = event.getSource();
+        //AccessibilityWindowInfo windowInfo = event.
+        if (parentNodeInfo == null) {
+            return;
+        }
+        //Log.d("window content", String.valueOf(parentNodeInfo));
+
+//        if (parentNodeInfo.getText() != null && parentNodeInfo.getText().length() > 0) {
+//
+//            String capturedText = parentNodeInfo.getText().toString();
+//            Log.d("text captured", capturedText);
+//        }
         String packageName = event.getPackageName().toString();
+
+        //get foreground app name
         PackageManager packageManager = this.getPackageManager();
         try {
             ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, 0);
             final CharSequence applicationLabel = packageManager.getApplicationLabel(applicationInfo);
             Log.e(TAG, "App name is: "+applicationLabel);
+            Toast.makeText(this, applicationLabel, Toast.LENGTH_SHORT);
 
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+
+        //dfs(parentNodeInfo);
+        //get all the child views from the nodeInfo
+        getChild(parentNodeInfo);
+
+        //fetch urls from different browsers
+        SupportedBrowserConfig browserConfig = null;
+        for (SupportedBrowserConfig supportedConfig: getSupportedBrowsers()) {
+            if (supportedConfig.packageName.equals(packageName)) {
+                browserConfig = supportedConfig;
+            }
+        }
+        //this is not supported browser, so exit
+        if (browserConfig == null) {
+            return;
+        }
+
+        String capturedUrl = captureUrl(parentNodeInfo, browserConfig);
+        parentNodeInfo.recycle();
+
+        //we can't find a url. Browser either was updated or opened page without url text field
+        if (capturedUrl == null) {
+            return;
+        }
+        Log.d("captured URL",capturedUrl);
+
+        long eventTime = event.getEventTime();
+        String detectionId = packageName + ", and url " + capturedUrl;
+        //noinspection ConstantConditions
+        long lastRecordedTime = previousUrlDetections.containsKey(detectionId) ? previousUrlDetections.get(detectionId) : 0;
+        //some kind of redirect throttling
+        if (eventTime - lastRecordedTime > 2000) {
+            previousUrlDetections.put(detectionId, eventTime);
+           // analyzeCapturedUrl(capturedUrl, browserConfig.packageName);
+        }
+
+
+
     }
+
+    public void dfs(AccessibilityNodeInfo info){
+        if(info == null)
+            return;
+        if(info.getText() != null && info.getText().length() > 0)
+            System.out.println(info.getText() + " class: "+info.getClassName());
+        for(int i=0;i<info.getChildCount();i++){
+            AccessibilityNodeInfo child = info.getChild(i);
+            dfs(child);
+            if(child != null){
+                child.recycle();
+            }
+        }
+    }
+
+
 
     @Override
     public void onInterrupt() {
@@ -44,4 +130,78 @@ public class MyService extends AccessibilityService {
 //        this.setServiceInfo(info);
 //        Log.e(TAG, "onServiceConnected: ");
 //    }
+    private void getChild(AccessibilityNodeInfo info)
+    {
+        int i=info.getChildCount();
+        for(int p=0;p<i;p++)
+        {
+            AccessibilityNodeInfo n=info.getChild(p);
+            if(n!=null) {
+                String strres = n.getViewIdResourceName();
+                if (n.getText() != null) {
+                    String txt = n.getText().toString();
+                    Log.d("Track", strres + "  :  " + txt);
+                }
+                getChild(n);
+            }
+        }
+    }
+
+    private static class SupportedBrowserConfig {
+        public String packageName, addressBarId;
+        public SupportedBrowserConfig(String packageName, String addressBarId) {
+            this.packageName = packageName;
+            this.addressBarId = addressBarId;
+        }
+    }
+
+    @NonNull
+    private static List<SupportedBrowserConfig> getSupportedBrowsers() {
+        List<SupportedBrowserConfig> browsers = new ArrayList<>();
+        browsers.add( new SupportedBrowserConfig("com.android.chrome", "com.android.chrome:id/url_bar"));
+        browsers.add( new SupportedBrowserConfig("org.mozilla.firefox", "org.mozilla.firefox:id/mozac_browser_toolbar_url_view"));
+        browsers.add( new SupportedBrowserConfig("com.opera.browser", "com.opera.browser:id/url_field"));
+        browsers.add( new SupportedBrowserConfig("com.opera.mini.native", "com.opera.mini.native:id/url_field"));
+        browsers.add( new SupportedBrowserConfig("com.duckduckgo.mobile.android", "com.duckduckgo.mobile.android:id/omnibarTextInput"));
+        browsers.add( new SupportedBrowserConfig("com.microsoft.emmx", "com.microsoft.emmx:id/url_bar"));
+        return browsers;
+    }
+
+    private String captureUrl(AccessibilityNodeInfo info, SupportedBrowserConfig config) {
+        List<AccessibilityNodeInfo> nodes = info.findAccessibilityNodeInfosByViewId(config.addressBarId);
+        if (nodes == null || nodes.size() <= 0) {
+            return null;
+        }
+        //System.out.println("nodeInfo"+ String.valueOf(info));
+        AccessibilityNodeInfo addressBarNodeInfo = nodes.get(0);
+        String url = null;
+        if (addressBarNodeInfo.getText() != null) {
+            url = addressBarNodeInfo.getText().toString();
+            Log.d("url from func", url);
+        }
+        addressBarNodeInfo.recycle();
+        return url;
+    }
+    private void analyzeCapturedUrl(@NonNull String capturedUrl, @NonNull String browserPackage) {
+        String redirectUrl = "your redirect url is here";
+        if (capturedUrl.contains("facebook.com")) {
+            performRedirect(redirectUrl, browserPackage);
+        }
+    }
+// we just reopen the browser app with our redirect url using service context
+    private void performRedirect(@NonNull String redirectUrl, @NonNull String browserPackage) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(redirectUrl));
+            intent.setPackage(browserPackage);
+            intent.putExtra(Browser.EXTRA_APPLICATION_ID, browserPackage);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        catch(ActivityNotFoundException e) {
+            // the expected browser is not installed
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(redirectUrl));
+            startActivity(i);
+        }
+    }
+
 }
